14609c673ab13a2702c48c581af10f05da38021d
Rick-Spar <s312316@student.salfordcc.ac.uk>
2025-03-27 16:29:54 +0000

    ADD PLAYER IMAGE

---
+++ src/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+public final class Constants {
+    private Constants (){
+        // prevents instantiation
+    }
+
+    public static final int BOARD_WIDTH = 640;
+    public static final int BOARD_HEIGHT = 480;
+
+    public static final String PLAYER_IMAGE_PATH = "resources/amogus.png";
+
+}
