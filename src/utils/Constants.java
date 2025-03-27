832d0b6b23c199e9e5977cf7525a03df325217bd
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2025-03-27 16:29:08 +0000

    Add player image

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -7,4 +7,6 @@ public final class Constants {
 
     public static final int BOARD_WIDTH = 640;
     public static final int BOARD_HEIGHT = 480;
+
+    public static final String PLAYER_IMAGE_PATH = "resources/amogus.png";
 }
