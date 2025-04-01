5153a55f4e74ef1f8dd19aaba7aa1282d9c40117
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2025-04-01 13:55:58 +0100

    Add player movement

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,6 @@
+import java.awt.event.KeyEvent;
+import java.util.Set;
+
 import static utils.Constants.*;
 
 public class Player extends Sprite {
@@ -8,5 +11,20 @@ public class Player extends Sprite {
     @Override
     public void tick() {
 
+    }
+
+    public void handleActiveKeys(Set<Integer> activeKeyCodes) {
+        if (activeKeyCodes.contains(KeyEvent.VK_UP)) {
+            getPos().y -= 10;
+        }
+        if (activeKeyCodes.contains(KeyEvent.VK_DOWN)) {
+            getPos().y += 10;
+        }
+        if (activeKeyCodes.contains(KeyEvent.VK_LEFT)) {
+            getPos().x -= 10;
+        }
+        if (activeKeyCodes.contains(KeyEvent.VK_RIGHT)) {
+            getPos().x += 10;
+        }
     }
 }
